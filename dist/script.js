d9716a46d28de278e7a88408276753bb4d9548df
sophiiiapapaya <swu241@ucr.edu>
2022-09-24 23:30:20 -0700

    added content from another yt tutorial
    https://youtu.be/5-_2z-DdWng
    Create another branch

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-const navTabs = document.querySelectorAll("#nav-tabs > a");
+/*const navTabs = document.querySelectorAll("#nav-tabs > a");
 navTabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     navTabs.forEach((tab) => {
@@ -7,3 +7,35 @@ navTabs.forEach((tab) => {
     tab.classList.add("active");
   });
 });
+*/
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'), /* id="nav-menu" in html file*/ 
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
