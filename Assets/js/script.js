08cd95c96d32d48d245262e47111f1e70352da1d
sophiiiapapaya <swu241@ucr.edu>
2022-09-26 14:55:23 -0700

    reorganize files in assets; use Swiper class in html, css, js files
    swiperjs.com
    moved css and js files into assets folder

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -39,3 +39,18 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== Swiper Projects ===============*/
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true, 
+  spaceBetween: 24, 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
