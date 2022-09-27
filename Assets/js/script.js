151f5f8a04b51309d53f6d3a19b95af0e8b4a5b1
sophiiiapapaya <swu241@ucr.edu>
2022-09-26 20:14:07 -0700

    Add contact section and setup contact form using emalijs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,7 +10,7 @@ navTabs.forEach((tab) => {
 */
 
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'), /* id="nav-menu" in html file*/ 
+const navMenu = document.getElementById('nav-menu'), // id="nav-menu" in html file 
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
@@ -41,6 +41,7 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== Swiper Projects ===============*/
+/* Css mode */
 let swiperProjects = new Swiper(".projects__container", {
   loop: true, 
   spaceBetween: 24, 
@@ -51,6 +52,54 @@ let swiperProjects = new Swiper(".projects__container", {
   pagination: {
     el: ".swiper-pagination",
   },
-  mousewheel: true,
-  keyboard: true,
+  /* responsive breakpoints */
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
 });
+
+/*================== Email js =================*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+  // check if the field has value
+  if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+    // Add and remove color
+    contactMessage.classList.remove('color-blue')
+    contactMessage.classList.add('color-red')
+
+    // Show message
+    contactMessage.textContent = 'All fields are required'
+  }
+  else{
+    // serviceID - templateID - #form (formID in html) - publickey
+    emailjs.sendForm('service_e2ep0ag', 'template_2duyxg8', '#contact-form', 'brFC5bE3CADv56IIZ')
+      .then(() =>{
+        // Show message and add color
+        contactMessage.classList.add('color-blue')
+        contactMessage.textContent = 'Message sent!'
+
+        // Remove message after 5 seconds
+        setTimeout(() =>{
+          contactMessage.textContent = ''
+        }, 5000)
+      }, (error) =>{
+        alert('OOPS! Something has failed...', error)
+      })
+
+    // To clear the input field
+    contactName.value = ''
+    contactEmail.value = ''
+    contactProject.value = ''
+  }
+}
+
+contactForm.addEventListener('submit', sendEmail) // button type is 'submit'
